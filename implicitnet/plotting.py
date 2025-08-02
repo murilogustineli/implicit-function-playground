55a2af155f6be05ad800d2f55d0fd512954d8a1a
Murilo Gustineli <murilogustineli@gmail.com>
2025-08-02 15:03:30 -0400

    updated 01-nb, added plots and animation gif

--- implicitnet/plotting.py
+++ implicitnet/plotting.py
@@ -42,24 +42,16 @@ def plot_model(
     y: np.array,
     predicted: np.array,
     title: str = "Linear Model",
-    xlim: list = [],
-    ylim: list = [],
-    blue_legend: str = "Linear Model",
 ) -> None:
-    fig, ax = plt.subplots(figsize=(6.4, 4.8), dpi=200)
-    if len(ylim) > 0:
-        ax.set_ylim(ylim)
-    elif len(xlim) > 0:
-        ax.set_xlim(xlim)
-    else:
-        ax.set_ylim([-1, 4.5])
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=120)
     ax.plot(x.numpy(), y.numpy(), "r-", linewidth=2.5, label="y=x^2")
-    ax.plot(x.numpy(), predicted, "b-", linewidth=2.5, label=blue_legend)
-    fig.suptitle(f"{title}", weight="bold", fontsize=16)
+    ax.plot(x.numpy(), predicted, "b-", linewidth=2.5, label="Linear Model")
+    ax.set_title(f"{title}", weight="bold", fontsize=16)
     ax.margins(x=0, y=0.1)  # No margins on x and y-axis
     ax.grid(color="blue", linestyle="--", linewidth=1, alpha=0.2)
-    ax.set_xlabel("X-axis", fontsize=12)
-    ax.set_ylabel("Y-axis", fontsize=12)
+    ax.set_xlabel("X-axis", fontsize=14)
+    ax.set_ylabel("Y-axis", fontsize=14)
+    ax.set_ylim([-1, 5])
     ax.legend(loc="upper right")
     ax.legend(loc="best")
     spines = ["top", "right", "bottom", "left"]
@@ -73,22 +65,22 @@ def plot_animation(
     x: np.array,
     y: np.array,
     preds: Dict[str, np.array],
-    file_name: str = "NonLinear",
-    folder_name: str = "non-linear-plots",
+    file_name: str = "Linear",
+    folder_name: str = "linear-plots",
     red_legend: str = "y=x^2",
     legend_loc: str = "best",
 ) -> None:
     for epoch, predicted in preds.items():
         path = make_directory("plots", f"{folder_name}")
-        fig, ax = plt.subplots(figsize=(6.4, 4.8), dpi=200)
-        ax.set_ylim([-1, 4.5])
+        fig, ax = plt.subplots(figsize=(8, 6), dpi=200)
         ax.plot(x.numpy(), y.numpy(), "r-", linewidth=2.5, label=red_legend)
-        ax.plot(x.numpy(), predicted, "b-", linewidth=2.5, label="Non-Linear Model")
-        fig.suptitle(f"Non-Linear Model, epoch={epoch}", weight="bold", fontsize=16)
+        ax.plot(x.numpy(), predicted, "b-", linewidth=2.5, label="Linear Model")
+        ax.set_title(f"Linear Model, epoch={epoch}", weight="bold", fontsize=16)
         ax.margins(x=0, y=0.1)  # No margins on x and y-axis
         ax.grid(color="blue", linestyle="--", linewidth=1, alpha=0.2)
-        ax.set_xlabel("X-axis", fontsize=12)
-        ax.set_ylabel("Y-axis", fontsize=12)
+        ax.set_xlabel("X-axis", fontsize=14)
+        ax.set_ylabel("Y-axis", fontsize=14)
+        ax.set_ylim([-1, 5])
         ax.legend(loc=legend_loc)
         spines = ["top", "right", "bottom", "left"]
         for s in spines:
